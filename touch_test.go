dee1bf090cf3a17b16123116deb567f753fbeaec
洪盟翔 <andyhorng168@gmail.com>
2016-04-19 18:16:50 +0800

    Add touch test

---
+++ touch_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type handler struct{}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	handleTouch(w, r)
+}
+
+func TestTouch(t *testing.T) {
+
+	h := handler{}
+
+	server := httptest.NewServer(h)
+	defer server.Close()
+
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Fail()
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fail()
+	}
+}
